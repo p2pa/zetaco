77c89489e94402d02c6a2f463d24514e88837a9a
p2pa <ppasta270@gmail.com>
2022-12-20 16:59:23 +0100

    Building out the frontend using vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,26 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div :class="[$store.state.layout_style, $store.state.menu_style]">
+      <component v-bind:is="layout"></component>
+  </div>
 </template>
+<script setup>
+  import { computed } from 'vue';
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+  import '@/assets/sass/app.scss';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  import { useMeta } from '@/composables/use-meta';
+  import { useStore } from 'vuex';
+
+  useMeta({ title: 'Sales Admin' });
+
+  const store = useStore();
+
+  const layout = computed(() => {
+      return store.getters.layout;
+  });
 </script>
+<script>
+  // layouts
+  import appLayout from './layouts/app-layout.vue';
+  import authLayout from './layouts/auth-layout.vue';
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  export default {
+      components: {
+          app: appLayout,
+          auth: authLayout,
+      },
+  };
+</script>
