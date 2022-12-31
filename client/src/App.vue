e56442d88dcd378bdd3fa71b6c3e8247358bd00a
pasta <ppasta270@gmail.com>
2022-12-31 12:26:04 +0100

    Added the option to toggle between table and chart results

--- client/src/App.vue
+++ client/src/App.vue
@@ -26,7 +26,7 @@
                                     <span class="input-group-text" id="basic-addon1">{{ inputTypes[index - 1] }}</span>
                                     <div class="form-control" v-for="values, i in inputValues[index - 1]" :key="i">
                                       <multiselect v-if="i == 0" v-model="inputValues[index - 1][0]" :options="options.chain.x" :searchable="true" :preselect-first="false" selected-label="" select-label="" deselect-label=""></multiselect>
-                                      <multiselect v-if="i == 1" v-model="inputValues[index - 1][1]" :options="inputValues[index - 1][0] == '' ? [] : options.chain.dimension[options.chain.x.find(inputValues[index - 1][0])]" :searchable="true" :preselect-first="false" selected-label="" select-label="" deselect-label=""></multiselect>                                    
+                                      <multiselect v-if="i == 1" v-model="inputValues[index - 1][1]" :options="inputValues[index - 1][0] == '' ? '' : options.chain.dimension[inputValues[index - 1][0]]" :searchable="true" :preselect-first="false" selected-label="" select-label="" deselect-label=""></multiselect>                                    
                                     </div>
                                 </div>
                                 <div class="input-group mb-4" v-if="inputTypes[index - 1] == 'filter'">
@@ -64,9 +64,31 @@
         <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 layout-spacing">
           <div class="widget widget-revenue">
               <div class="widget-heading">
-                  <h5>Chart</h5>
+                  <h5>Results</h5>
                   
                   <div class="dropdown btn-group">
+                      <a v-if="table" class="btn dropdown-toggle btn-icon-only" style="margin-right:15px;" @click="toggleTable()"> 
+                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="background-color:#0E1726; color:#fff"
+                        version="1.1" width="24" height="24" viewBox="0 0 256 256" xml:space="preserve" fill="none"
+                              stroke="white">
+                        <defs>
+                        </defs>
+                        <g style="stroke-width: 0; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: none; fill-rule: nonzero; opacity: 1;" transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)" >
+                          <path d="M 87.994 0 H 69.342 c -1.787 0 -2.682 2.16 -1.418 3.424 l 5.795 5.795 l -33.82 33.82 L 28.056 31.196 l -3.174 -3.174 c -1.074 -1.074 -2.815 -1.074 -3.889 0 L 0.805 48.209 c -1.074 1.074 -1.074 2.815 0 3.889 l 3.174 3.174 c 1.074 1.074 2.815 1.074 3.889 0 l 15.069 -15.069 l 14.994 14.994 c 1.074 1.074 2.815 1.074 3.889 0 l 1.614 -1.614 c 0.083 -0.066 0.17 -0.125 0.247 -0.202 l 37.1 -37.1 l 5.795 5.795 C 87.84 23.34 90 22.445 90 20.658 V 2.006 C 90 0.898 89.102 0 87.994 0 z" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: #888ea8; fill-rule: nonzero; opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
+                          <path d="M 65.626 37.8 v 49.45 c 0 1.519 1.231 2.75 2.75 2.75 h 8.782 c 1.519 0 2.75 -1.231 2.75 -2.75 V 23.518 L 65.626 37.8 z" style="stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill:#888ea8; fill-rule: nonzero; opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
+                          <path d="M 47.115 56.312 V 87.25 c 0 1.519 1.231 2.75 2.75 2.75 h 8.782 c 1.519 0 2.75 -1.231 2.75 -2.75 V 42.03 L 47.115 56.312 z" style="stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: #888ea8; fill-rule: nonzero; opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
+                          <path d="M 39.876 60.503 c -1.937 0 -3.757 -0.754 -5.127 -2.124 l -6.146 -6.145 V 87.25 c 0 1.519 1.231 2.75 2.75 2.75 h 8.782 c 1.519 0 2.75 -1.231 2.75 -2.75 V 59.844 C 41.952 60.271 40.933 60.503 39.876 60.503 z" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: #888ea8; fill-rule: nonzero; opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
+                          <path d="M 22.937 46.567 L 11.051 58.453 c -0.298 0.298 -0.621 0.562 -0.959 0.8 V 87.25 c 0 1.519 1.231 2.75 2.75 2.75 h 8.782 c 1.519 0 2.75 -1.231 2.75 -2.75 V 48.004 L 22.937 46.567 z" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: #888ea8; fill-rule: nonzero; opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
+                        </g>
+                        </svg>
+                      </a>
+                      <a v-if="!table" class="btn dropdown-toggle btn-icon-only" style="margin-right:15px;" @click="toggleTable()"> 
+                        <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 122.88 102.52" style="enable-background:new 0 0 122.88 102.52" xml:space="preserve" fill="#888ea8">
+                          <g>
+                            <path class="st0" d="M5.42,0h112.04c2.98,0,5.42,2.44,5.42,5.42V97.1c0,2.98-2.44,5.42-5.42,5.42H5.42c-2.98,0-5.42-2.44-5.42-5.42 V5.42C0,2.44,2.44,0,5.42,0L5.42,0z M8.48,23.58H38.1c0.82,0,1.48,0.67,1.48,1.48v9.76c0,0.81-0.67,1.48-1.48,1.48H8.48 c-0.81,0-1.48-0.67-1.48-1.48v-9.76C6.99,24.25,7.66,23.58,8.48,23.58L8.48,23.58z M84.78,82.35h29.63c0.82,0,1.48,0.67,1.48,1.48 v9.76c0,0.81-0.67,1.48-1.48,1.48H84.78c-0.81,0-1.48-0.67-1.48-1.48v-9.76C83.29,83.02,83.96,82.35,84.78,82.35L84.78,82.35z M46.8,82.35h29.28c0.82,0,1.48,0.67,1.48,1.48v9.76c0,0.81-0.67,1.48-1.48,1.48H46.8c-0.81,0-1.48-0.67-1.48-1.48v-9.76 C45.31,83.02,45.98,82.35,46.8,82.35L46.8,82.35z M8.48,82.35H38.1c0.82,0,1.48,0.67,1.48,1.48v9.76c0,0.81-0.67,1.48-1.48,1.48 H8.48c-0.81,0-1.48-0.67-1.48-1.48v-9.76C6.99,83.02,7.66,82.35,8.48,82.35L8.48,82.35z M84.78,62.76h29.63 c0.82,0,1.48,0.67,1.48,1.48V74c0,0.81-0.67,1.48-1.48,1.48H84.78c-0.81,0-1.48-0.67-1.48-1.48v-9.76 C83.29,63.43,83.96,62.76,84.78,62.76L84.78,62.76z M46.8,62.76h29.28c0.82,0,1.48,0.67,1.48,1.48V74c0,0.81-0.67,1.48-1.48,1.48 H46.8c-0.81,0-1.48-0.67-1.48-1.48v-9.76C45.31,63.43,45.98,62.76,46.8,62.76L46.8,62.76z M8.48,62.76H38.1 c0.82,0,1.48,0.67,1.48,1.48V74c0,0.81-0.67,1.48-1.48,1.48H8.48c-0.81,0-1.48-0.67-1.48-1.48v-9.76 C6.99,63.43,7.66,62.76,8.48,62.76L8.48,62.76z M84.78,43.17h29.63c0.82,0,1.48,0.67,1.48,1.48v9.76c0,0.81-0.67,1.48-1.48,1.48 H84.78c-0.81,0-1.48-0.67-1.48-1.48v-9.76C83.29,43.84,83.96,43.17,84.78,43.17L84.78,43.17z M46.8,43.17h29.28 c0.82,0,1.48,0.67,1.48,1.48v9.76c0,0.81-0.67,1.48-1.48,1.48H46.8c-0.81,0-1.48-0.67-1.48-1.48v-9.76 C45.31,43.84,45.98,43.17,46.8,43.17L46.8,43.17z M8.48,43.17H38.1c0.82,0,1.48,0.67,1.48,1.48v9.76c0,0.81-0.67,1.48-1.48,1.48 H8.48c-0.81,0-1.48-0.67-1.48-1.48v-9.76C6.99,43.84,7.66,43.17,8.48,43.17L8.48,43.17z M84.78,23.58h29.63 c0.82,0,1.48,0.67,1.48,1.48v9.76c0,0.81-0.67,1.48-1.48,1.48H84.78c-0.81,0-1.48-0.67-1.48-1.48v-9.76 C83.29,24.25,83.96,23.58,84.78,23.58L84.78,23.58z M46.8,23.58h29.28c0.82,0,1.48,0.67,1.48,1.48v9.76c0,0.81-0.67,1.48-1.48,1.48 H46.8c-0.81,0-1.48-0.67-1.48-1.48v-9.76C45.31,24.25,45.98,23.58,46.8,23.58L46.8,23.58z"/>
+                          </g>
+                        </svg>
+                      </a>
                       <a href="javascript:;" id="ddlRevenue" class="btn dropdown-toggle btn-icon-only" data-bs-toggle="dropdown" aria-expanded="false">
                           <svg
                               xmlns="http://www.w3.org/2000/svg"
@@ -93,7 +115,73 @@
 
               <div class="widget-content">
                   <!-- <div class="chart-title">Total Profit <span class="text-primary ms-1">$10,840</span></div> -->
-                  <apex-chart v-if="revenue_options" height="325" :type="chartType" :options="revenue_options" :series="revenue_series"></apex-chart>
+                  <div v-if="!table">
+                    <apex-chart v-if="revenue_options" height="325" :type="chartType" :options="revenue_options" :series="revenue_series"></apex-chart>
+                  </div>
+
+                  <div v-if="table && tableData.length > 0">
+                    <div class="VueTables VueTables--client">
+                      <div class="row">
+                        <div class="col-md-12">                        
+                          <div class="form-group form-inline pull-right VueTables__limit">
+                            <div class="VueTables__limit-field">                            
+                              <select id="VueTables__limit_0svA4" class="form-control">
+                                <option value="5">5</option>
+                                <option value="10">10</option>
+                                <option value="20">20</option>
+                                <option value="50">50</option>
+                              </select>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <table class="VueTables__table table" summary="false" v-for="x in tableData" :key="x">
+                        <thead class="">
+                          <tr>
+                            <th class="VueTables__sortable " title="" tabindex="0" v-for="column in x.columns" :key="column">
+                              <span class="VueTables__heading">{{ column }}</span>
+                            </th>                          
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr class="VueTables__row" v-for="row in x.rows" :key="row">                          
+                            <td tabindex="0" class="">
+                              {{ row[0]}}
+                            </td>
+                            <td tabindex="0" class="">
+                              {{ row[1]}}
+                            </td>
+                            <td tabindex="0" class="">
+                              {{ row[2]}}
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                      <div class="VuePagination row col-md-12 ">
+                        <nav class="text-center">
+                          <ul class="VuePagination__pagination pagination VuePagination__pagination" style="">
+                            <li class="VuePagination__pagination-item VuePagination__pagination-item page-item VuePagination__pagination-item VuePagination__pagination-item-prev-page page-item  disabled VuePagination__pagination-item-prev-page disabled">
+                              <button type="button" class="page-link" disabled="">&lt;</button>
+                            </li>
+                            <li class="VuePagination__pagination-item VuePagination__pagination-item page-item active">
+                              <button class="page-link active">1</button>
+                            </li>
+                            <li class="VuePagination__pagination-item VuePagination__pagination-item page-item ">
+                              <button class="page-link ">2</button>
+                            </li>
+                            <li class="VuePagination__pagination-item VuePagination__pagination-item page-item ">
+                              <button class="page-link ">3</button>
+                            </li>
+                            <li class="VuePagination__pagination-item VuePagination__pagination-item page-item VuePagination__pagination-item  VuePagination__pagination-item-next-page page-item   VuePagination__pagination-item-next-page ">
+                              <button type="button" class="page-link">&gt;</button>
+                            </li>
+                          </ul>
+                          <p class="VuePagination__count VuePagination__count text-center col-md-12" style="">Showing 1 to 10 of 27</p>
+                        </nav>
+                      </div>
+
+                    </div>
+                  </div>
               </div>
             </div>
           </div>  
@@ -118,98 +206,12 @@ export default {
     Multiselect,
     ApexChart
   },
-  methods: {
-    fetchQuery: async function(){
-      let from;
-      let to = parseInt((new Date().getTime() / 1000).toFixed(0)); 
-
-      // to do
-      // check if all values contain valid entry      
-      
-      // if inputTypes contains a filter
-      if(this.inputTypes.indexOf('filter')){
-        this.inputValues[this.inputTypes.indexOf('filter')].forEach(el => {
-            let condition = el.split(" ");
-            if(condition[0] == 'Date'){
-              if(condition[1] == '>='){
-                from = parseInt(new Date(condition[2]).getTime() / 1000);
-              }
-            }
-          });
-      }
-
-      let dataArray = []
-
-      for (let index = 0; index < this.inputTypes.length; index++) {
-        const element = this.inputTypes[index];           
-        if(element == 'protocol'){
-          let array = this.inputValues[index];
-          // if type == revenue do bar chart         
-
-          let url = 'http://localhost:3000/api/' + array[0] + '/' + array[1] + '/' + from + '/' + to ;      
-
-          await axios
-          .get(url)
-          .then((res) => {
-            dataArray.push({                 
-                name: array[0], 
-                data: res.data[0].data                         
-            })
-            this.revenue_options.labels = res.data[0].labels
-          })   
-        } 
-        
-        if(element == 'chain'){
-          let array = this.inputValues[index];
-          // if type == revenue do bar chart         
-
-          let url = 'http://localhost:3000/api/chains/' + array[0] + '/' + array[1].replace(/\s+/, "")  + '/' + from + '/' + to ;      
-          
-          await axios
-          .get(url)
-          .then((res) => {
-            let item = {                 
-                name: array[0], 
-                data: []                         
-            };
-            let labels = []
-            for (let z = 0; z < res.data.rows.length; z++) {
-              let el = res.data.rows[z];
-              labels.push(el[1])
-              item.data.push(el[2])
-            }
-            dataArray.push(item)
-            this.revenue_options.labels = labels
-          })   
-        }       
-      }      
-      console.log(this.revenue_options.labels);      
-      this.revenue_series = dataArray;
-    },
-  },  
-  async beforeMount(){
-    // get chains
-    let url = 'http://localhost:3000/getChains'; 
-    let nameArray = [];
-    let dimensionArray = [];
-    await axios
-    .get(url)
-    .then((res) => {
-      let chains = res.data;
-      for (let z = 0; z < chains.length; z++) {
-        let el = chains[z];
-        nameArray.push(el.name)
-        dimensionArray.push(el.dimensions)       
-      }
-    })   
-    this.options.chain.x = nameArray;
-    this.options.chain.dimension = dimensionArray;
-    console.log(this.options)
-  },
   data: function() {
     return {        
+        table: false,
+        tableData: [],
         chartType: "area",
-        inputsAmount: 1,
+        inputsAmount: 2,
         inputTypes: [
           'chain',
           'filter'
@@ -259,7 +261,7 @@ export default {
               tickAmount: 7,
               labels: {
                   formatter: function (value) {
-                    if(value > 1000000000){
+                    if(value > 100000000){
                       return Math.floor((value / 1000000000) * 100) / 100 + 'B';
                     }
                     if(value > 1000000){
@@ -305,5 +307,101 @@ export default {
         }
       }
   },  
+  methods: {
+    toggleTable: function(){
+      this.table = !this.table;
+    },
+    fetchQuery: async function(){
+      let from;
+      let to = parseInt((new Date().getTime() / 1000).toFixed(0)); 
+
+      // to do
+      // check if all values contain valid entry      
+      
+      // if inputTypes contains a filter
+      if(this.inputTypes.indexOf('filter')){
+        this.inputValues[this.inputTypes.indexOf('filter')].forEach(el => {
+            let condition = el.split(" ");
+            if(condition[0] == 'Date'){
+              if(condition[1] == '>='){
+                from = parseInt(new Date(condition[2]).getTime() / 1000);
+              }
+            }
+          });
+      }
+
+      let dataArray = []
+
+      for (let index = 0; index < this.inputTypes.length; index++) {
+        const element = this.inputTypes[index];           
+        if(element == 'protocol'){
+          let array = this.inputValues[index];
+          // if type == revenue do bar chart         
+
+          let url = 'http://localhost:3000/api/' + array[0] + '/' + array[1] + '/' + from + '/' + to ;      
+
+          await axios
+          .get(url)
+          .then((res) => {
+            dataArray.push({                 
+                name: array[0], 
+                data: res.data[0].data                         
+            })
+            this.revenue_options.labels = res.data[0].labels
+          })   
+        } 
+        
+        if(element == 'chain'){
+          let array = this.inputValues[index];
+          // if type == revenue do bar chart         
+
+          let url = 'http://localhost:3000/api/chains/' + array[0] + '/' + array[1].replace(/\s+/, "")  + '/' + from + '/' + to ;      
+          
+          await axios
+          .get(url)
+          .then((res) => {
+            // fill table data
+            this.tableData.push(res.data);
+            // 
+            let item = {                 
+                name: array[0], 
+                data: []                         
+            };
+            let labels = []
+            for (let z = 0; z < res.data.rows.length; z++) {
+              let el = res.data.rows[z];
+              
+              labels.push(el[1])
+              item.data.push(el[2])
+            }
+            dataArray.push(item)
+            this.revenue_options.labels = labels
+          })   
+        }       
+      }      
+      console.log(this.revenue_options.labels);      
+      this.revenue_series = dataArray;
+    },
+  },  
+  async beforeMount(){
+    // get chains
+    let url = 'http://localhost:3000/getChains'; 
+    let nameArray = [];
+    let dimensionArray = [];
+    await axios
+    .get(url)
+    .then((res) => {
+      let chains = res.data;
+      for (let z = 0; z < chains.length; z++) {
+        let el = chains[z];
+        nameArray.push(el.name)
+        dimensionArray[el.name] = el.dimensions;
+      }
+    })   
+    this.options.chain.x = nameArray;
+    this.options.chain.dimension = dimensionArray;
+    console.log(this.options)
+  },
+  
 }
 </script>
