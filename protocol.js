f38100b3461755358f46ecc03f0561d47c479fe0
pasta <ppasta270@gmail.com>
2023-01-02 23:13:06 +0100

    Added sales endpoint to blur, opensea and x2y2

--- protocol.js
+++ protocol.js
@@ -14,6 +14,7 @@ class Flipside {
 
 module.exports = function(project){
     this.chosen = project;
+
     this.flipside = new Flipside(
         process.env.SHROOM_API_KEY,
         "https://node-api.flipsidecrypto.com"
@@ -21,13 +22,13 @@ module.exports = function(project){
 
     this.available = [{
         name: 'blur',
-        dimensions: ['Buyers', 'Sellers']
+        dimensions: ['Buyers', 'Sellers', 'Sales']
     },{
         name: 'opensea',
-        dimensions: ['Buyers', 'Sellers']
+        dimensions: ['Buyers', 'Sellers', 'Sales']
     },{
         name: 'x2y2',
-        dimensions: ['Buyers', 'Sellers']
+        dimensions: ['Buyers', 'Sellers', 'Sales']
     }]    
 
     this.getProtocols = function(){
@@ -35,38 +36,73 @@ module.exports = function(project){
     }
     
     this.getData = async function(from, to, protocol, dimension){
-        var data = await this.flipside.query.run({
-            sql: `select
-                    DATE_TRUNC('day', block_timestamp) AS date,
-                    count(distinct ${dimension == 'buyers' ? 'buyer_address' : dimension == 'sellers' ? 'seller_address' : ''})
-                from
-                    ethereum.core.ez_nft_sales
-                where
-                    platform_name = '${protocol}'
-                    and block_timestamp >= '2022-01-01'
-                    and PRICE_USD is not null
-                group by
-                    1
-                order by
-                    1`,
-            ttlMinutes: 10
-        });
-
-        var finished = {
-            columns: ["blockchain", "date", dimension],
-            rows: [],
-        };
-
-        for (let i = 0; i <  data.rows.length; i++) {
-            let el =  data.rows[i];
-            //console.log(el)
-            let inUnix = this.convertToUnix(el[0]);
-            if(inUnix > from && inUnix < to){
-                //console.log("Pushed into finished array: " + el[1] + '/' + el[0] + '/' + el[4])
-                finished.rows.push([this.chosen, el[0], el[1]])
-            }            
-        }
-        return finished; 
+        if(dimension == 'buyer' || dimension == 'seller'){
+            var data = await this.flipside.query.run({
+                sql: `select
+                        DATE_TRUNC('day', block_timestamp) AS date,
+                        count(distinct ${dimension == 'buyers' ? 'buyer_address' : dimension == 'sellers' ? 'seller_address' : ''})
+                    from
+                        ethereum.core.ez_nft_sales
+                    where
+                        platform_name = '${protocol}'
+                        and block_timestamp >= '2022-01-01'
+                        and PRICE_USD is not null
+                    group by
+                        1
+                    order by
+                        1`,
+                ttlMinutes: 10
+            });
+    
+            var finished = {
+                columns: ["blockchain", "date", dimension],
+                rows: [],
+            };
+    
+            for (let i = 0; i <  data.rows.length; i++) {
+                let el =  data.rows[i];
+                //console.log(el)
+                let inUnix = this.convertToUnix(el[0]);
+                if(inUnix > from && inUnix < to){
+                    //console.log("Pushed into finished array: " + el[1] + '/' + el[0] + '/' + el[4])
+                    finished.rows.push([this.chosen, el[0], el[1]])
+                }            
+            }
+            return finished; 
+        }    
+        if(dimension == 'sales'){
+            var data = await this.flipside.query.run({
+                sql: `select
+                        DATE_TRUNC('day', block_timestamp) AS date,
+                        count(Distinct tx_hash) as sales
+                    from
+                        ethereum.core.ez_nft_sales
+                    where
+                        platform_name = '${protocol}'
+                        and date >= '2022-01-01'
+                    group by
+                        1
+                    order by
+                        1`,
+                ttlMinutes: 10
+            });
+    
+            var finished = {
+                columns: ["blockchain", "date", dimension],
+                rows: [],
+            };
+    
+            for (let i = 0; i <  data.rows.length; i++) {
+                let el =  data.rows[i];
+                //console.log(el)
+                let inUnix = this.convertToUnix(el[0]);
+                if(inUnix > from && inUnix < to){
+                    //console.log("Pushed into finished array: " + el[1] + '/' + el[0] + '/' + el[4])
+                    finished.rows.push([this.chosen, el[0], el[1]])
+                }            
+            }
+            return finished; 
+        }    
     }
 
     this.getBuyers = async function(from, to){
@@ -75,6 +111,10 @@ module.exports = function(project){
 
     this.getSellers = async function(from, to){
         return this.getData(from, to, this.chosen, 'sellers');        
+    }
+
+    this.getSales = async function(from, to){
+        return this.getData(from, to, this.chosen, 'sales');        
     }
     
     this.convertToUnix = function(date){
